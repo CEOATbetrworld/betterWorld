ddfc02b287e11642175128574c78212ce523fe30
DHRUV KUMAR <dkiphones620@gmail.com>
2018-03-10 23:57:51 +0530

    contactus page

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,38 @@
 var i = 0;
-var list = ["Humans help Humans", "Humans help animals", "Humans can help anyone who accepts", "This is what makes us human"];
 
-function World(props) {
-    return <h1>{props.being}</h1>;
+var list = [
+  "Humans help Humans",
+  "Humans help animals",
+  "Humans can help anyone who accepts",
+  "This is what makes us human"
+];
+
+class World extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      being: list[0]
+    };
+  }
+
+  componentDidMount() {
+    var self = this;
+    setInterval(function() {
+      if (i < list.length) i++;
+      if (i === list.length) i = 0;
+      self.setState({
+        being: list[i]
+      });
+    }, 1300);
+  }
+  render() {
+    return (
+      <div>
+        <h1>{this.state.being}</h1>
+        <a href="contact.html">Contact Us</a>
+      </div>
+    );
+  }
 }
 
-setInterval(function () {
-    ReactDOM.render(<World being={list[i]} />, document.getElementById("root"));
-    if (i < list.length) i++;
-    if (i === list.length) i = 0;
-}, 1500);
+ReactDOM.render(<World />, document.getElementById("root"));
