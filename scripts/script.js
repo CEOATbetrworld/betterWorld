8ebb0e133fb3d05ab3481abf95c1ce160234f11a
DHRUV KUMAR <dkiphones620@gmail.com>
2018-03-18 12:48:45 +0530

    improved styling

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,44 @@ var list = [
   "उसे इन्सान कहते हैं"
 ];
 
+class Head extends React.Component {
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          BetterWorld
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">
+                Home <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="contact.html">
+                Contact Us
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
 class World extends React.Component {
   constructor(props) {
     super(props);
@@ -19,7 +57,7 @@ class World extends React.Component {
 
   componentDidMount() {
     var self = this;
-    setInterval(function () {
+    setInterval(function() {
       if (i < list.length) i++;
       if (i === list.length) i = 0;
       self.setState({
@@ -27,14 +65,17 @@ class World extends React.Component {
       });
     }, 1300);
   }
+
   render() {
     return (
-      <div>
-        <div id='stars'></div>
-        <div id='stars2'></div>
-        <div id='stars3'></div>
-        <h1>{this.state.being}</h1>
-        <a href="contact.html">Contact Us</a>
+      <div className="container-fluid">
+        <Head />
+          <div className="row center">
+            <h1>{this.state.being}</h1>
+        </div>
+        <div className="contact row">
+          <a className="col"href="">Contact Us</a>
+        </div>
       </div>
     );
   }
